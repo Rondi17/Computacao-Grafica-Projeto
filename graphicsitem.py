a887bb965b1c24a62dcf9cea0ad66c33b99055a5
Rondinelly Martins <128706832+Rondi17@users.noreply.github.com>
2023-08-20 16:00:13 -0300

    Add files via upload

--- graphicsitem.py
+++ graphicsitem.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QGraphicsLineItem, QGraphicsEllipseItem
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QPoint
 
 #obs: colocar tipo
 class GraphicsItem:
@@ -11,8 +11,10 @@ class Ponto(QGraphicsEllipseItem):
     def __init__(self, x, y):
         super().__init__(x, y, 10, 10)
         self.setBrush(Qt.black)
+        self.name = "Ponto"
 
 class Reta(QGraphicsLineItem):
-    def __init__(self, x1, y1):
-        super().__init__(x1, y1, 700, 250)
+    def __init__(self, x1, y1, x2, y2):
+        super().__init__(x1, y1, x2, y2)
+        self.name = "Reta"
         
--- window.py
+++ window.py
@@ -1,9 +1,19 @@
 import PyQt5.QtWidgets as qtw
+from PyQt5 import QtGui, QtCore, QtWidgets
 from PyQt5.QtWidgets import QMainWindow, QPushButton, QGraphicsView, QGraphicsScene, QGraphicsEllipseItem
-from PyQt5.QtCore import Qt, QSize
-from PyQt5.QtWidgets import QWidget, QLabel, QLineEdit, QPushButton, QInputDialog
+from PyQt5.QtCore import Qt, QSize, QPoint
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 from graphicsitem import Ponto, Reta
 from window2 import DialogBox
+
+class Resize(QWidget):
+    def paintEvent(self, event):
+        QP= QtGui.QPainter(self)
+        pen = QtGui.QPen(QtGui.QColor(QtCore.Qt.black), 5)
+        QP.setPen(pen)
+        QP.drawRect()
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -18,6 +28,8 @@ class MainWindow(QMainWindow):
         self.height = 600
         self.initUI()
 
+        self.factor = 1.25
+
     def initUI(self):
         self.setWindowTitle(self.title)
         #setGeometry(x inicial,y inicial,)
@@ -25,21 +37,50 @@ class MainWindow(QMainWindow):
         #self.setFixedSize(QSize(400, 300))
         self.create_viewport()
         self.create_buttons()
+        self.create_objects_widget()
         self.draw_objects()
         self.show()
+
+    def create_objects_widget(self):
+        self.objects_widget = QListWidget(self)
+        self.objects_widget.setGeometry(0, 0, 200, 200)
+        self.objects_widget.show()
+
+    def draw_display_file(self, object):
+        objeto = QListWidgetItem(self.objects_widget)
+        objeto.setText(object.name)
+        self.objects_widget.addItem(objeto)
+        #button.clicked.connect(self.showElement_onViewPort)
+        self.objects_widget.update()
+        #cria a viewport  e desenha os objetos na tela de acordo com o x e y calculados com a formula
+        #objetos do display_file
 
     def create_buttons(self):
         #botao novo objeto
         botao1 = QPushButton('Novo Objeto', self)
-        botao1.move(100, 100)
+        botao1.move(100, 200)
         botao1.resize(90, 30)
         #botao1.setStyleSheet('QPushButton {background-color:#0FB328;font:bold; font-size:20px}')
         botao1.clicked.connect(self.get_object_information)
 
     def create_viewport(self):
         self.scene = QGraphicsScene()
-        view = QGraphicsView(self.scene, self)
-        view.setGeometry(200,0,600,500)
+        self.view = QGraphicsView(self.scene, self)
+        self.view.setGeometry(200,0,600,500)
+
+        QtWidgets.QShortcut(
+            QtGui.QKeySequence(QtGui.QKeySequence.ZoomIn),
+            self.view,
+            context=QtCore.Qt.WidgetShortcut,
+            activated=self.zoom_in,
+        )
+
+        QtWidgets.QShortcut(
+            QtGui.QKeySequence(QtGui.QKeySequence.ZoomOut),
+            self.view,
+            context=QtCore.Qt.WidgetShortcut,
+            activated=self.zoom_out,
+        )
 
     #tela para criar um novo objeto
     def get_object_information(self):
@@ -59,8 +100,13 @@ class MainWindow(QMainWindow):
         if info['opcao'] == "Ponto":
             new_object = Ponto(info['x'], info['y'])
             self.display_file.append(new_object)
+            self.draw_display_file(new_object)
+            self.scene.addItem(new_object)
         elif info["opcao"] == "Reta":
             print("criar reta")
+            new_object = Reta(info['x1'], info['y1'], info['x2'], info['y2'])
+            self.draw_display_file(new_object)
+            self.scene.addItem(new_object)
         else:
             print("outra coisa")
 
@@ -68,3 +114,22 @@ class MainWindow(QMainWindow):
         for object in self.display_file:
             self.scene.addItem(object)
 
+
+    @QtCore.pyqtSlot()
+    def zoom_in(self):
+        scale_tr = QtGui.QTransform()
+        scale_tr.scale(self.factor, self.factor)
+
+        tr = self.view.transform() * scale_tr
+        self.view.setTransform(tr)
+
+    @QtCore.pyqtSlot()
+    def zoom_out(self):
+        scale_tr = QtGui.QTransform()
+        scale_tr.scale(self.factor, self.factor)
+
+        scale_inverted, invertible = scale_tr.inverted()
+
+        if invertible:
+            tr = self.view.transform() * scale_inverted
+            self.view.setTransform(tr)
